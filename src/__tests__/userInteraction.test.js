c97a282f559d0cce91a68fb425ad71ba5a865144
Ashe546 <asfyd42@gmail.com>
2022-07-22 09:17:15 -0300

    fixed linter error

--- src/__tests__/userInteraction.test.js
+++ src/__tests__/userInteraction.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Navbar from '../components/Screen';
-import HomeView from '../components/Home';
-import Calculator from '../components/Calculator'
 
 describe('navbar component', () => {
   it('should render correctly', () => {
@@ -13,21 +11,3 @@ describe('navbar component', () => {
     expect(tree).toMatchSnapshot();
   });
 });
-
-describe('home view', () => {
-  it('should render correctly', () => {
-    const tree = renderer
-      .create(<BrowserRouter> <HomeView /> </BrowserRouter>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
-
-describe('home view', () => {
-    it('should render correctly', () => {
-      const tree = renderer
-        .create(<BrowserRouter> <Calculator/> </BrowserRouter>)
-        .toJSON();
-      expect(tree).toMatchSnapshot();
-    });
-  });
